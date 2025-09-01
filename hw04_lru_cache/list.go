96c5044daba2913e4802a933b0cc92f02ecab3ce
anastasiaDAmber <anastasiaDAmber@yandex.ru>
2025-09-01 21:03:19 +0300

    HW №4 IS COMPLETED

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,98 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head *ListItem
+	tail *ListItem
+	len  int
 }
 
 func NewList() List {
-	return new(list)
+	return &list{}
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{Value: v, Next: l.head}
+	if l.head != nil {
+		l.head.Prev = item
+	} else {
+		// список был пуст
+		l.tail = item
+	}
+	l.head = item
+	l.len++
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{Value: v, Prev: l.tail}
+	if l.tail != nil {
+		l.tail.Next = item
+	} else {
+		// список был пуст
+		l.head = item
+	}
+	l.tail = item
+	l.len++
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil || l.len == 0 {
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		// удаление первого элемента списка
+		l.head = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		// удаление последнего элемента списка
+		l.tail = i.Prev
+	}
+
+	i.Next = nil
+	i.Prev = nil
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == nil || l.head == i {
+		return
+	}
+	// вырез i
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		// i был хвостом
+		l.tail = i.Prev
+	}
+	// перемещение в начало списка
+	i.Prev = nil
+	i.Next = l.head
+	if l.head != nil {
+		l.head.Prev = i
+	}
+	l.head = i
+	if l.tail == nil { // если список был пуст (не уверена, что нужна данная проверка)
+		l.tail = i
+	}
 }
